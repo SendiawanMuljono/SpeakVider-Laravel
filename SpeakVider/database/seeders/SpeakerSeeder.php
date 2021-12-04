0ef037e10de393ee6d69989ef4406fe831ed2596
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2021-12-04 21:44:37 +0700

    Update SpeakerSeeder.php

--- SpeakVider/database/seeders/SpeakerSeeder.php
+++ SpeakVider/database/seeders/SpeakerSeeder.php
@@ -17,7 +17,8 @@ class SpeakerSeeder extends Seeder
         $speakers = [
             ['email' => 'speaker1@speakvider.com', 'password' => 'speaker1', 'phoneNumber' => '0000', 'cv' => 'speaker1cv.jpg', 'photo' => 'speaker1.jpg', 'skill' => 'speaker'],
             ['email' => 'speaker2@speakvider.com', 'password' => 'speaker2', 'phoneNumber' => '0001', 'cv' => 'speaker2cv.jpg', 'photo' => 'speaker2.jpg', 'skill' => 'speaker'],
-            ['email' => 'speaker3@speakvider.com', 'password' => 'speaker3', 'phoneNumber' => '0002', 'cv' => 'speaker3cv.jpg', 'photo' => 'speaker3.jpg', 'skill' => 'speaker']
+            ['email' => 'speaker3@speakvider.com', 'password' => 'speaker3', 'phoneNumber' => '0002', 'cv' => 'speaker3cv.jpg', 'photo' => 'speaker3.jpg', 'skill' => 'speaker'],
+            ['email' => 'speaker4@speakvider.com', 'password' => 'speaker4', 'phoneNumber' => '0003', 'cv' => 'speaker4cv.jpg', 'photo' => 'speaker4.jpg', 'skill' => 'speaker']
         ];
 
         DB::table('speakers')->insert($speakers);

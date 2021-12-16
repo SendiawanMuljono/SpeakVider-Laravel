c77e207918ef7df08cb7932d7a87263706d18309
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2021-12-16 19:34:57 +0700

    Update home.blade.php

--- SpeakVider/resources/views/home.blade.php
+++ SpeakVider/resources/views/home.blade.php
@@ -2,28 +2,6 @@
 
 @section('yieldPlace')
     <div class="container-fluid mb-5 mt-5">
-        <div class="d-flex justify-content-center mb-3">
-            <h3 class="bg-black text-white pt-1 pb-1 pl-4 pr-4 font-weight-bold">OUR SPEAKERS</h3>
-        </div>
-        <div class="d-flex justify-content-end mb-2">
-            <a href="/allspeakers">SEE MORE ></a>
-        </div>
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-            @foreach ($speakers as $speaker)
-                <div class="col d-flex justify-content-center mb-5">
-                    <div class="card" style="width: 18rem;">
-                        <a href="/aboutus">
-                            <img src="{{ asset('assets/speakers/'.$speaker->photo) }}" class="card-img-top" width="250px" height="250px">
-                            <div class="card-body d-flex justify-content-center">
-                                <h5 class="card-title">{{ $speaker->name }}</h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-    <div class="container-fluid mb-5 mt-5">
         <div class="d-flex justify-content-center mt-5 mb-5">
             <h3 class="bg-black text-white pt-1 pb-1 pl-4 pr-4 font-weight-bold">ABOUT US</h3>
         </div>
@@ -39,6 +17,29 @@
                     </a>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="container-fluid mb-5 mt-5">
+        <div class="d-flex justify-content-center mb-3">
+            <h3 class="bg-black text-white pt-1 pb-1 pl-4 pr-4 font-weight-bold">OUR SPEAKERS</h3>
+        </div>
+        <div class="d-flex justify-content-end mb-2">
+            <a href="/allspeakers">SEE MORE ></a>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+            @foreach ($speakers as $speaker)
+                <div class="col d-flex justify-content-center mb-5">
+                    <div class="card" style="width: 18rem;">
+                        <a href="#">
+                            <img src="{{ asset('assets/speakers/'.$speaker->photo) }}" class="card-img-top" width="250px" height="250px">
+                            <div class="card-body d-flex justify-content-center">
+                                <h5 class="card-title">{{ $speaker->name }}</h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection

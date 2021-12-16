0b2bbc310f9878eb36efb379d384bfe70cacbd54
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2021-12-16 19:52:49 +0700

    make ScheduleSeeder

--- SpeakVider/database/seeders/DatabaseSeeder.php
+++ SpeakVider/database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
         $this->call(SpeakerSeeder::class);
+        $this->call(ScheduleSeeder::class);
     }
 }

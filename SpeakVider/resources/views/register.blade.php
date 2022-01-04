ce18008fe58426f045096ddcb1898c7ef702c473
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-05 02:02:35 +0700

    Update register.blade.php

--- SpeakVider/resources/views/register.blade.php
+++ SpeakVider/resources/views/register.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="{{ asset('/assets/css/register.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
 </head>
 <body>
 
@@ -29,16 +28,6 @@
                         <a class="nav-link" href="/register">Register</a>
                     </li>
                 </ul>
-
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item ms-3">
-                        need
-                    </li>
-                    <li class="nav-item ms-1 text-primary">
-                         Help?
-                    </li>
-                </ul>
-
             </div>
         </div>
     </nav>
